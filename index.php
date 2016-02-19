491264da8dd2340be1b9a99ae6105759cd3dab3d
Yorik van Havre <yorik@uncreated.net>
2016-02-19 17:43:43 -0200

    Fixed background appearing in wiki

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ bind_textdomain_codeset("homepage", 'UTF-8');
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
-<html lang="<?php echo $lang;?>">
+<html lang="<?php echo $lang;?>" class="home">
     
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
