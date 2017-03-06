51b2c502bc6b8edbed3ab7e5ea83358dba7764c0
Kunda <luzpaz@users.noreply.github.com>
2017-03-05 19:29:10 -0500

    Modified mantis placeholder text for Attention Deficit folks
    Modified mantis placeholder text for Attention Deficit folks
    
    + changed some of the whitespace formatting to be more viewable on FF

--- tracker/plugins/FreecadTheme/freecad.js
+++ tracker/plugins/FreecadTheme/freecad.js
@@ -16,24 +16,25 @@ function addPlaceholderText(el, txt) {
 function addReportTemplate() {
   var summaryText = `Short description about the issue`;
 
-  var descText = `Explain the details of the bug/feature/patch. Make sure to \
-explain what you were doing when it happened, what you expected to happen, and \
-what actually was the result.`;
+  var descText = `==> Explain the details of the bug/feature/patch. \
+What you were doing when it happened? \
+what you expected to happen? \
+what actually was the result?`;
 
-  var stepsText = `Please produce a step-by-step list (as detailed as possible) \
+  var stepsText = `==> Detailed step-by-step list how to recreate the bug \
 to help the devs and testers reproduce your issue.
 Ex:
-1. Launch FreeCAD
-2. Open Sketcher through the dropdown
-3. Open a New Document: File > New
-4. etc...
+1. Launch FreeCAD 
+2. Open Sketcher through the dropdown 
+3. Open a New Document: File > New 
+4. etc... 
 Result: Summarize the result you see happening in FreeCAD`;
 
-  var infoText = `Please paste the contents of Help > About FreeCAD > "Copy to \
-clipboard"
+  var infoText = `==> Please paste the contents of Help > About FreeCAD > "Copy to \
+clipboard" 
 
-If you are running a debug release you can also publish a debugging backtrace. \
-Please see: http://www.freecadweb.org/wiki/Debugging
+==> Running a debug release? Publish a debugging backtrace: \
+Please see: http://www.freecadweb.org/wiki/Debugging 
 
 If you have a file to share that is larger than the FreeCAD tracker allows, you \
 can upload the file to a cloud-based 3rd party service (like NextCloud, Dropbox, \
