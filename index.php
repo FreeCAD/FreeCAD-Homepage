9bd311a1408b773e3900728a5443701e4025ec30
AR795 <17176341+AR795@users.noreply.github.com>
2018-07-30 09:18:23 +0400

    Removed Piwik code

--- index.php
+++ index.php
@@ -48,22 +48,6 @@ bind_textdomain_codeset("homepage", 'UTF-8');
 		
 		<!-- Custom Styles -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-        
-        <!-- Piwik -->
-        <script type="text/javascript">
-          var _paq = _paq || [];
-          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
-          _paq.push(['enableLinkTracking']);
-          (function() {
-            var u="//analytics.freecad.io/";
-            _paq.push(['setTrackerUrl', u+'piwik.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-          })();
-        </script>
-        <!-- End Piwik Code -->
 	</head>
 
 	<body>
