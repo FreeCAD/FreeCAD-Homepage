a62df0d68b1dd13f8871619e851f227148c4c05f
AR795 <17176341+AR795@users.noreply.github.com>
2018-10-18 11:04:34 +0400

    Update xml-to-json.php

---
+++ xml-to-json.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Create plain PHP associative array from XML.
+ *
+ * Example usage:
+ *   $xmlNode = simplexml_load_file('example.xml');
+ *   $arrayData = xmlToArray($xmlNode);
+ *   echo json_encode($arrayData);
+ *
+ * @param SimpleXMLElement $xml The root node
+ * @param array $options Associative array of options
+ * @return array
+ * @link http://outlandishideas.co.uk/blog/2012/08/xml-to-json/ More info
+ * @author Tamlyn Rhodes <http://tamlyn.org>
+ * @license http://creativecommons.org/publicdomain/mark/1.0/ Public Domain
+ * https://github.com/tamlyn/xml2json/blob/master/xml2json.php
+ */
+
+// Obtain Feed Url From POST
+$feed_url = $_GET['url'];
+//$feed_url = 'http://softplug.com/feed';
+
+if(!$_GET['url'])
+$feed_url = 'http://news.yahoo.com/rss';
+//$feed_url = 'http://'.$feed_url;
+
+$callback= $_GET['callback'];
+
+  $xmlNode = simplexml_load_file($feed_url);
+  $arrayData = xmlToArray($xmlNode);
+
+  if($callback) {
+    header('Content-Type: application/javascript; charset=utf-8');
+    echo $callback. '(' . json_encode($arrayData) . ')';  // jsonP callback
+  } else {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($arrayData);
+  }
+
+function xmlToArray($xml, $options = array()) {
+    $defaults = array(
+        'namespaceSeparator' => ':',//you may want this to be something other than a colon
+        'attributePrefix' => '',   //to distinguish between attributes and nodes with the same name
+        //'attributePrefix' => '@',   //to distinguish between attributes and nodes with the same name
+        'alwaysArray' => array(),   //array of xml tag names which should always become arrays
+        'autoArray' => true,        //only create arrays for tags which appear more than once
+        'textContent' => '$',       //key used for the text content of elements
+        'autoText' => true,         //skip textContent key if node has no attributes or child nodes
+        'keySearch' => false,       //optional search and replace on tag and attribute names
+        'keyReplace' => false       //replace values for above search values (as passed to str_replace())
+    );
+    $options = array_merge($defaults, $options);
+    $namespaces = $xml->getDocNamespaces();
+    $namespaces[''] = null; //add base (empty) namespace
+ 
+    //get attributes from all namespaces
+    $attributesArray = array();
+    foreach ($namespaces as $prefix => $namespace) {
+        foreach ($xml->attributes($namespace) as $attributeName => $attribute) {
+            //replace characters in attribute name
+            if ($options['keySearch']) $attributeName =
+                    str_replace($options['keySearch'], $options['keyReplace'], $attributeName);
+            $attributeKey = $options['attributePrefix']
+                    . ($prefix ? $prefix . $options['namespaceSeparator'] : '')
+                    . $attributeName;
+            $attributesArray[$attributeKey] = (string)$attribute;
+        }
+    }
+ 
+    //get child nodes from all namespaces
+    $tagsArray = array();
+    foreach ($namespaces as $prefix => $namespace) {
+        foreach ($xml->children($namespace) as $childXml) {
+            //recurse into child nodes
+            $childArray = xmlToArray($childXml, $options);
+            list($childTagName, $childProperties) = each($childArray);
+ 
+            //replace characters in tag name
+            if ($options['keySearch']) $childTagName =
+                    str_replace($options['keySearch'], $options['keyReplace'], $childTagName);
+            //add namespace prefix, if any
+            if ($prefix) $childTagName = $prefix . $options['namespaceSeparator'] . $childTagName;
+ 
+            if (!isset($tagsArray[$childTagName])) {
+                //only entry with this key
+                //test if tags of this type should always be arrays, no matter the element count
+                $tagsArray[$childTagName] =
+                        in_array($childTagName, $options['alwaysArray']) || !$options['autoArray']
+                        ? array($childProperties) : $childProperties;
+            } elseif (
+                is_array($tagsArray[$childTagName]) && array_keys($tagsArray[$childTagName])
+                === range(0, count($tagsArray[$childTagName]) - 1)
+            ) {
+                //key already exists and is integer indexed array
+                $tagsArray[$childTagName][] = $childProperties;
+            } else {
+                //key exists so convert to integer indexed array with previous value in position 0
+                $tagsArray[$childTagName] = array($tagsArray[$childTagName], $childProperties);
+            }
+        }
+    }
+ 
+    //get text content of node
+    $textContentArray = array();
+    $plainText = trim((string)$xml);
+    if ($plainText !== '') $textContentArray[$options['textContent']] = $plainText;
+ 
+    //stick it all together
+    $propertiesArray = !$options['autoText'] || $attributesArray || $tagsArray || ($plainText === '')
+            ? array_merge($attributesArray, $tagsArray, $textContentArray) : $plainText;
+ 
+    //return node as array
+    return array(
+        $xml->getName() => $propertiesArray
+    );
+}
