07ee5b23d67a2583e128690ad852ee13593ebcdd
Kurt Kremitzki <kurt@kwk.systems>
2019-11-17 23:10:26 -0600

    Try forcing active FTP connection

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 filename=$(echo "$1" | sed 's/^\.\///')
 url="ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_HOST}/public_html/$filename"
-curl --ftp-skip-pasv-ip --ftp-ssl --ftp-create-dirs -T "$filename" "$url"
+curl --disable-eprt -P --ftp-ssl --ftp-create-dirs -T "$filename" "$url"
