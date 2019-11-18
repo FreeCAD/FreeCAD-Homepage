7706fc81c6cd45c36f9bb67438193d566f020251
Kurt Kremitzki <kurt@kwk.systems>
2019-11-17 23:40:47 -0600

    Try skip passive ip again

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 filename=$(echo "$1" | sed 's/^\.\///')
-url="ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_HOST}/public_html/$filename"
-curl --ftp-ssl --ftp-create-dirs -T "$filename" "$url"
+url="sftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_HOST}/public_html/$filename"
+curl --ftp-skip-pasv-ip --ftp-ssl --ftp-create-dirs -T "$filename" "$url"
