e65c0ccf4975a789b9716078e8980059f1bd443a
Kurt Kremitzki <kurt@kwk.systems>
2019-11-17 19:56:42 -0600

    Use wput for deployment instead

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-find .. -not -path '../.git*' -not -path '../scripts*' -not -path \
-    '../.travis.yml' -exec curl --ftp-ssl --ftp-create-dirs -T {} \
-    "ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_HOST}/public_html/" \;
+wput .htaccess * "ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_HOST}/public_html/"
