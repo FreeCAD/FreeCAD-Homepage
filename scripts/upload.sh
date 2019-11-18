16970e008d6aec4d93f933678c925d27f8df31f4
Kurt Kremitzki <kurt@kwk.systems>
2019-11-17 22:51:45 -0600

    Try different auth method

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 filename=$(echo "$1" | sed 's/^\.\///')
-url="ftp://${FTP_USER}:${FTP_PASSWORD}@${FTP_HOST}/public_html/$filename"
-curl --ftp-ssl --ftp-create-dirs -T "$filename" "$url"
+url="ftp://${FTP_HOST}/public_html/$filename"
+curl --user "${FTP_USER}:${FTP_PASSWORD}" --ftp-ssl --ftp-create-dirs \
+    -T "$filename" "$url"
