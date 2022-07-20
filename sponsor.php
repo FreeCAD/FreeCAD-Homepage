0cf1ad96d1b596e2ce957b490dcee37632f390ce
Yorik van Havre <yorik@uncreated.net>
2022-07-20 09:13:13 +0200

    Updated sponsors

--- sponsor.php
+++ sponsor.php
@@ -228,6 +228,7 @@
           
           <div class="col-lg-7 text-light text-center text-lg-left px-md-4">
           
+              <div class="bronze sponsor">Ian Rees</div>
               <div class="bronze sponsor">Ton Roosendaal</div>
               <div class="bronze sponsor">Lukas Alberts</div>
           
