4d108c98aca15f146b57a65765a56a7e78b9aff2
Yorik van Havre <yorik@uncreated.net>
2024-07-04 12:19:39 +0200

    Updated sponsors

--- sponsor.php
+++ sponsor.php
@@ -275,6 +275,7 @@
 
           <div class="col-lg-7 text-light text-center text-lg-left px-md-4">
 
+              <div class="bronze sponsor">Amplituda d.o.o.</div>
               <div class="bronze sponsor">plasmapotential</div>
               <div class="bronze sponsor">RobotMachines</div>
               <div class="bronze sponsor">Damian Toczek</div>
