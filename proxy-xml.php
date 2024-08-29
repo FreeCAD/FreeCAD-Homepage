388d1ac6e302e8209e985592522ea322d17c6ba2
Reqrefusion <39885728+Reqrefusion@users.noreply.github.com>
2024-08-29 15:01:24 +0300

    Blog in the House
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Fine adjustment.

---
+++ proxy-xml.php
@@ -0,0 +1,15 @@
+﻿<?php
+if (isset($_GET['url'])) {
+    $url = $_GET['url'];
+
+    if (filter_var($url, FILTER_VALIDATE_URL)) {
+        $response = file_get_contents($url);
+
+        header('Content-Type: application/xml');
+        echo $response;
+    } else {
+        header("HTTP/1.1 400 Bad Request");
+    }
+} else {
+    header("HTTP/1.1 400 Bad Request");
+}
