92e2505ade181995df1585d85d235d8da76b73f5
Reqrefusion <39885728+Reqrefusion@users.noreply.github.com>
2024-10-15 17:10:54 +0300

    Update sponsor.php
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Update sponsor.php

--- sponsor.php
+++ sponsor.php
@@ -2,8 +2,58 @@
     $currentpage = "sponsor.php";
     include("header.php");
 ?>
+    <script>
+function updateLatestCategoryFromFeed(xmlUrl, titleId, bodyId, buttonId, imageId) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', xmlUrl, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var parser = new DOMParser();
+      var xml = parser.parseFromString(xhr.responseText, 'application/xml');
+      
+      var latestEventsPost = xml.getElementsByTagName('item')[0];
 
-    <div id="main" class="container-fluid whitelinks">
+      var sectionTitle = document.getElementById(titleId);
+      var sectionBody = document.getElementById(bodyId);
+      var learnMoreButton = document.getElementById(buttonId);
+      var imageElement = document.getElementById(imageId);
+
+      if (latestEventsPost) {
+        sectionTitle.textContent = latestEventsPost.getElementsByTagName('title')[0].textContent;
+        sectionBody.innerHTML = latestEventsPost.getElementsByTagName('description')[0].textContent;
+        learnMoreButton.setAttribute('href', latestEventsPost.getElementsByTagName('link')[0].textContent);
+
+        var contentEncoded = latestEventsPost.getElementsByTagName('content:encoded')[0]?.textContent || '';
+        var tempDiv = document.createElement('div');
+        tempDiv.innerHTML = contentEncoded;
+        var firstImage = tempDiv.querySelector('img');
+        var imageUrl = firstImage ? firstImage.src : '';
+
+        if (!imageUrl) {
+          imageUrl = 'images/Development-Updates.avif';
+        }
+
+        if (imageElement) {
+          imageElement.setAttribute('src', imageUrl);
+        }
+      }
+    }
+  };
+  xhr.send();
+}
+
+updateLatestCategoryFromFeed(
+  'proxy-xml.php?url=https://blog.freecad.org/category/grants/feed/', 
+  'grants-title', 
+  'grants-description', 
+  'grants-link',
+  'grants-image'
+);
+
+
+    </script>
+    
+    <div id="main" class="container-fluid">
 
         <div class="download-notes text-center">
             <h2 class="downloads-notes-title"><?php echo _('Donating and sponsoring'); ?></h2>
@@ -63,7 +113,7 @@
 
 
 
-        <section class="row section d-flex justify-content-around">
+        <section class="row section d-flex justify-content-around whitelinks">
 
           <div class="col-lg-4">
             <h3><?php echo _('One-time donation'); ?></h3>
@@ -88,7 +138,7 @@
 
 
 
-        <section class="row section d-flex justify-content-around">
+        <section class="row section d-flex justify-content-around whitelinks">
 
           <div class="col-lg-4">
             <h3><?php echo _('Sponsoring'); ?></h3>
@@ -155,7 +205,7 @@
         </section>
 
 
-        <section class="row section d-flex justify-content-around">
+        <section class="row section d-flex justify-content-around whitelinks">
           <div class="col-lg-4">
             <h3><?php echo _('Gold sponsors'); ?></h3>
           </div>
@@ -192,7 +242,7 @@
           </div>
         </section>
 
-        <section class="row section d-flex justify-content-around">
+        <section class="row section d-flex justify-content-around whitelinks">
           <div class="col-lg-4">
             <h3><?php echo _('Early sponsors'); ?></h3>
           </div>
@@ -234,7 +284,7 @@
           </div>
         </section>
 
-        <section class="row section d-flex justify-content-around">
+        <section class="row section d-flex justify-content-around whitelinks">
           <div class="col-lg-4">
             <h3><?php echo _('Silver sponsors'); ?></h3>
           </div>
@@ -242,7 +292,7 @@
 
                 <div class="silver sponsor">
                     <a href="https://route4me.com">Route4Me Route Planner</a>
-                     – trusted for last-mile route planning and route optimization.
+                     - trusted for last-mile route planning and route optimization.
                 </div>
 
                 <div class="silver sponsor">
@@ -273,7 +323,7 @@
           </div>
         </section>
 
-        <section class="row section d-flex justify-content-around">
+        <section class="row section d-flex justify-content-around whitelinks">
           <div class="col-lg-4">
             <h3><?php echo _('Bronze sponsors'); ?></h3>
           </div>
@@ -304,6 +354,28 @@
           </div>
         </section>
 
+
+
+        <section class="row section d-flex align-items-center justify-content-around rounded mb-5">
+          <div class="col-lg-5 rounded model-backround p-2 ">
+              <div class="placeholder-glow">
+                  <img id="grants-image" class="img-fluid" alt="Grant Image" loading="lazy">
+              </div>
+          </div>
+          <div class="col-lg-6 text-light text-center text-lg-start rounded text-backround">
+              <h3 id="grants-title" class="section-title mt-3 placeholder-glow">
+                  <span class="placeholder col-6 bg-secondary"></span>
+              </h3>
+              <p id="grants-description" class="section-body placeholder-glow">
+                  <span class="placeholder col-12 bg-secondary"></span>
+                  <span class="placeholder col-8 bg-secondary"></span>
+                  <span class="placeholder col-10 bg-secondary"></span>
+              </p>
+              <a id="grants-link" href="#" class="btn btn-light rounded-pill mt-3">
+                  <?php echo _('Learn more'); ?>
+              </a>
+          </div>
+        </section>
     </div>
 
 <?php include 'footer.php'; ?>
