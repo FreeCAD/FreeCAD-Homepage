24cac94fa0576adce4e60202a4bd07ef62235ab2
Reqrefusion <39885728+Reqrefusion@users.noreply.github.com>
2024-11-21 00:15:16 +0300

    Update downloads.php

--- downloads.php
+++ downloads.php
@@ -12,6 +12,52 @@
             window.location = durl;
             return false;
         }
+function updateLatestCategoryFromFeed(xmlUrl, titleId, bodyId, buttonId, imageId) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', xmlUrl, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var parser = new DOMParser();
+      var xml = parser.parseFromString(xhr.responseText, 'application/xml');
+
+      var latestEventsPost = xml.getElementsByTagName('item')[0];
+
+      var sectionTitle = document.getElementById(titleId);
+      var sectionBody = document.getElementById(bodyId);
+      var learnMoreButton = document.getElementById(buttonId);
+      var imageElement = document.getElementById(imageId);
+
+      if (latestEventsPost) {
+        sectionTitle.textContent = latestEventsPost.getElementsByTagName('title')[0].textContent;
+        sectionBody.innerHTML = latestEventsPost.getElementsByTagName('description')[0].textContent;
+        learnMoreButton.setAttribute('href', latestEventsPost.getElementsByTagName('link')[0].textContent);
+
+        var contentEncoded = latestEventsPost.getElementsByTagName('content:encoded')[0]?.textContent || '';
+        var tempDiv = document.createElement('div');
+        tempDiv.innerHTML = contentEncoded;
+        var firstImage = tempDiv.querySelector('img');
+        var imageUrl = firstImage ? firstImage.src : '';
+
+        if (!imageUrl) {
+          imageUrl = 'images/Development-Updates.avif';
+        }
+
+        if (imageElement) {
+          imageElement.setAttribute('src', imageUrl);
+        }
+      }
+    }
+  };
+  xhr.send();
+}
+
+updateLatestCategoryFromFeed(
+  'proxy-xml.php?url=https://blog.freecad.org/category/releases/feed/', 
+  'releases-title', 
+  'releases-description', 
+  'releases-link',
+  'releases-image'
+);
     </script>
 
     <div id="main" class="container-fluid">
@@ -162,7 +208,26 @@
       </p>
     </div>
 
-
+        <section class="row section d-flex align-items-center justify-content-around rounded mb-5">
+          <div class="col-lg-5 rounded model-backround p-2 ">
+              <div class="placeholder-glow">
+                  <img id="releases-image" class="img-fluid" alt="Release Image" loading="lazy">
+              </div>
+          </div>
+          <div class="col-lg-6 text-light text-center text-lg-start rounded text-backround">
+              <h3 id="releases-title" class="section-title mt-3 placeholder-glow">
+                  <span class="placeholder col-6 bg-secondary"></span>
+              </h3>
+              <p id="releases-description" class="section-body placeholder-glow">
+                  <span class="placeholder col-12 bg-secondary"></span>
+                  <span class="placeholder col-8 bg-secondary"></span>
+                  <span class="placeholder col-10 bg-secondary"></span>
+              </p>
+              <a id="releases-link" href="#" class="btn btn-light rounded-pill mt-3">
+                  <?php echo _('Learn more'); ?>
+              </a>
+          </div>
+        </section>
     </div>
 
 <?php include 'footer.php'; ?>
