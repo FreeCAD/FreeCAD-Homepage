2c6b1e025afbfb653cb2164a0bf75cf55206fcce
Chris Hennes <chennes@pioneerlibrarysystem.org>
2025-02-19 16:11:06 -0600

    Convert from distinct_id to uuid

--- deletetelemetry.php
+++ deletetelemetry.php
@@ -1,7 +1,19 @@
 <?php
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://eu.posthog.com/api/projects/51229/persons/{$_GET["person_id"]}?delete_events=true");
+
+    $unique_id = $_GET["person_id"];
+    $project_id = 51229;
+    
+    curl_setopt($ch, CURLOPT_URL, "https://eu.posthog.com/api/projects/{$project_id}/persons/?distinct_id={$distinct_id}");
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json", "Authorization: Bearer {$_SERVER["POSTHOG_API_KEY"]}"]);
+    $result = curl_exec($ch);
+    $data = json_decode($result, true);
+
+    $uuid = $data['results'][0]['uuid'] ?? null;
+
+    curl_setopt($ch, CURLOPT_URL, "https://eu.posthog.com/api/projects/{$project_id}/persons/{$uuid}?delete_events=true");
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
     curl_setopt($ch, CURLOPT_HTTPHEADER, ["Authorization: Bearer {$_SERVER["POSTHOG_API_KEY"]}"]);
     $result = curl_exec($ch);
