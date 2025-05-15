403d33cb8dfe16dbc0789ae58e02163467f5f920
Chris Hennes <chennes@pioneerlibrarysystem.org>
2025-05-15 15:42:31 -0500

    Update downloads.php to 1.0.1

--- downloads.php
+++ downloads.php
@@ -67,7 +67,7 @@ updateLatestCategoryFromFeed(
         <!-- Major+Minor Version of FC Stable -->
         <!-- -------------------------------- -->
 
-        <h2 class="downloads-notes-title"><?php echo _('Current stable version:'); ?> 1.0.0</h2>
+        <h2 class="downloads-notes-title"><?php echo _('Current stable version:'); ?> 1.0.1</h2>
         <p><?php echo _('Select your desired platform (note that all downloads are for 64-bit systems):'); ?></p>
 
       </div>
@@ -83,8 +83,8 @@ updateLatestCategoryFromFeed(
               <img class="w-100 p-4" src="svg/icon-windows.svg" alt="Windows">
               <h3 class="card-title download-platform-name m-0 pb-3">Windows</h3>
               <div class="flex-column flex-lg-row">
-                <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-Windows-x86_64-installer-1.exe">x86_64 installer</a>
-                <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-Windows-x86_64-py311.7z">x86_64 portable (.7z)</a>
+                <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.1/FreeCAD_1.0.1-conda-Windows-x86_64-installer-1.exe">x86_64 installer</a>
+                <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.1/FreeCAD_1.0.1-conda-Windows-x86_64-py311.7z">x86_64 portable (.7z)</a>
               </div>
             </div>
             <div class="card-footer px-xl-5 py-xl-4">
@@ -105,8 +105,8 @@ updateLatestCategoryFromFeed(
             <div class="card-body d-block align-items-center text-center px-xl-5 py-xl-4">
               <img class="w-100 p-4" src="svg/icon-apple.svg" alt="Mac">
               <h3 class="card-title download-platform-name m-0 pb-3">Mac</h3>
-              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-macOS-arm64-py311.dmg">Apple Silicon</a>
-              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-macOS-x86_64-py311.dmg">Intel</a>
+              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.1/FreeCAD_1.0.1-conda-macOS-arm64-py311.dmg">Apple Silicon</a>
+              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.1/FreeCAD_1.0.1-conda-macOS-x86_64-py311.dmg">Intel</a>
             </div>
             <div class="card-footer px-xl-5 py-xl-4">
               <small class="text-muted">
@@ -126,8 +126,8 @@ updateLatestCategoryFromFeed(
             <div class="card-body d-block align-items-center text-center px-xl-5 py-xl-4">
               <img class="w-100 p-4" src="svg/icon-linux.svg" alt="Linux">
               <h3 class="card-title download-platform-name m-0 pb-3">Linux</h3>
-              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-Linux-x86_64-py311.AppImage">x86_64 AppImage</a>
-              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.0/FreeCAD_1.0.0-conda-Linux-aarch64-py311.AppImage">aarch64 AppImage</a>
+              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.1/FreeCAD_1.0.1-conda-Linux-x86_64-py311.AppImage">x86_64 AppImage</a>
+              <a class="btn btn-primary rounded-pill my-1" onclick="thankyou(event)" role="button" href="https://github.com/FreeCAD/FreeCAD/releases/download/1.0.1/FreeCAD_1.0.1-conda-Linux-aarch64-py311.AppImage">aarch64 AppImage</a>
             </div>
             <div class="card-footer px-xl-5 py-xl-4">
               <small class="text-muted">
