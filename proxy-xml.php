265b49065078d5a536bc07d519aa9221039e7bed
Anany <52584554+alanEG@users.noreply.github.com>
2025-07-14 02:47:33 -0700

    Update proxy-xml.php
    Security improvement

--- proxy-xml.php
+++ proxy-xml.php
@@ -1,15 +1,38 @@
-﻿<?php
-if (isset($_GET['url'])) {
-    $url = $_GET['url'];
+<?php
+$whitelist = [
+    'blog.freecad.org'
+];
 
-    if (filter_var($url, FILTER_VALIDATE_URL)) {
-        $response = file_get_contents($url);
-
-        header('Content-Type: application/xml');
-        echo $response;
-    } else {
-        header("HTTP/1.1 400 Bad Request");
+function is_url_safe($url, $whitelist) {
+    $parsed = parse_url($url);
+    if (!$parsed || !isset($parsed['host'])) {
+        return false;
     }
-} else {
-    header("HTTP/1.1 400 Bad Request");
+   
+    $host = strtolower($parsed['host']);
+   
+    if (in_array($host, $whitelist)) {
+        return true;
+    }
+   
+    $ip = gethostbyname($host);
+    if ($ip === $host) {
+        return false;
+    }
+   
+    if (in_array($ip, $whitelist)) {
+        return true;
+    }
+   
+    return false;
 }
+
+$url = $_GET['url'] ?? '';
+if (is_url_safe($url, $whitelist)) {
+    $response = file_get_contents($url);
+    header('Content-Type: application/xml');
+    echo $response;
+} else {
+    echo "URL not allowed";
+}
+?>
