4222c5bc98e41afd518e7e911ddd15d25ca8ea8b
Chris Hennes <chennes@pioneerlibrarysystem.org>
2025-08-30 17:11:48 -0500

    Generate JSON lang input instead of all PHP

--- translation.php
+++ translation.php
@@ -1,34 +1,43 @@
 <?php
 
-$localeMap = array(
-    'en' => 'en_US',
-    'be' => 'be_BY',
-    'ca' => 'ca_ES',
-    'cs' => 'cs_CZ',
-    'de' => 'de_DE',
-    'el' => 'el_GR',
-    'es' => 'es_ES',
-    'eu' => 'eu_ES',
-    'fa' => 'fa_IR',
-    'fi' => 'fi_FI',
-    'fr' => 'fr_FR',
-    'hr' => 'hr_HR',
-    'hu' => 'hu_HU',
-    'it' => 'it_IT',
-    'ja' => 'ja_JP',
-    'ka' => 'ka_GE',
-    'nl' => 'nl_NL',
-    'pl' => 'pl_PL',
-    'pt' => 'pt_BR',
-    'ru' => 'ru_RU',
-    'sl' => 'sl_SI',
-    'sr' => 'sr_RS',
-    'sv' => 'sv_SE',
-    'tr' => 'tr_TR',
-    'uk' => 'uk_UA',
-    'zh' => 'zh_CN',
-    'zh' => 'zh_TW',
-);
+function loadLocaleMap($filePath)
+{
+    if (!file_exists($filePath)) {
+        throw new Exception("File not found: $filePath");
+    }
+
+    $jsonData = file_get_contents($filePath);
+    $localeMap = json_decode($jsonData, true);
+
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        throw new Exception("Invalid JSON in $filePath: " . json_last_error_msg());
+    }
+
+    return $localeMap;
+}
+
+function getFlags($href = '/')
+{
+    $localeMap = loadLocaleMap(__DIR__ . '/localeMap.json');
+
+    // Default English entry
+    echo('<a class="dropdown-item" href="' . $href . '">
+            <img src="lang/en/flag.jpg" alt="" />' . _('English') . '</a>');
+
+    foreach ($localeMap as $shortCode => $locale) {
+        if ($shortCode === 'en') {
+            continue;
+        }
+        $localeObj = locale_get_display_language($locale, $locale);
+        if (!$localeObj) {
+            $localeObj = $locale; // fallback
+        }
+
+        echo('<a class="dropdown-item" href="' . $href . '?lang=' . $shortCode . '">
+                <img src="lang/' . $shortCode . '/flag.jpg" alt="" />' . _($localeObj) . '</a>');
+    }
+}
+
 
 function getLangParam(): string {
     if (empty($_GET['lang'])) {
@@ -39,6 +48,7 @@ function getLangParam(): string {
 }
 
 $lang = getLangParam();
+$localeMap = loadLocaleMap(__DIR__ . '/localeMap.json');
 $locale = isset($localeMap[$lang]) ? $localeMap[$lang] : $lang;
 putenv("LC_ALL=$locale");
 setlocale(LC_ALL, $locale);
@@ -56,40 +66,9 @@ if (!file_exists('lang/'.$flagcode."/flag.jpg")) {
 $langStr    = "?lang=" . urlencode($lang);
 $langattrib = "&lang=" . urlencode($lang);
 
-function getFlags($href='/') {
-    echo('						<a class="dropdown-item" href="'.$href.'"><img src="lang/en/flag.jpg" alt="" />'._('English').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=be"><img src="lang/be/flag.jpg" alt="" />'._('Belarusian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=ca"><img src="lang/ca/flag.jpg" alt="" />'._('Catalan').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=cs"><img src="lang/cs/flag.jpg" alt="" />'._('Czech').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=de"><img src="lang/de/flag.jpg" alt="" />'._('German').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=el"><img src="lang/el/flag.jpg" alt="" />'._('Greek').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=es_CO"><img src="lang/es_CO/flag.jpg" alt="" />'._('Spanish').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=es_ES"><img src="lang/es_ES/flag.jpg" alt="" />'._('Spanish').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=eu"><img src="lang/eu/flag.jpg" alt="" />'._('Basque').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=fa"><img src="lang/fa/flag.jpg" alt="" />'._('Persian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=fi"><img src="lang/fi/flag.jpg" alt="" />'._('Finnish').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=fr"><img src="lang/fr/flag.jpg" alt="" />'._('French').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=hr"><img src="lang/hr/flag.jpg" alt="" />'._('Croatian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=hu"><img src="lang/hu/flag.jpg" alt="" />'._('Hungarian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=it"><img src="lang/it/flag.jpg" alt="" />'._('Italian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=ja"><img src="lang/ja/flag.jpg" alt="" />'._('Japanese').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=ka"><img src="lang/ka/flag.jpg" alt="" />'._('Georgian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=nl"><img src="lang/nl/flag.jpg" alt="" />'._('Dutch').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=pl"><img src="lang/pl/flag.jpg" alt="" />'._('Polish').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=pt_BR"><img src="lang/pt_BR/flag.jpg" alt="" />'._('Portuguese').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=ru"><img src="lang/ru/flag.jpg" alt="" />'._('Russian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=sl"><img src="lang/sl/flag.jpg" alt="" />'._('Slovenian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=sr"><img src="lang/sr/flag.jpg" alt="" />'._('Serbian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=sr_CS"><img src="lang/sr_CS/flag.jpg" alt="" />'._('Serbian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=sv_SE"><img src="lang/sv_SE/flag.jpg" alt="" />'._('Swedish').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=tr"><img src="lang/tr/flag.jpg" alt="" />'._('Turkish').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=uk"><img src="lang/uk/flag.jpg" alt="" />'._('Ukrainian').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=zh_CN"><img src="lang/zh_CN/flag.jpg" alt="" />'._('Chinese').'</a>');
-    echo('						<a class="dropdown-item" href="'.$href.'?lang=zh_TW"><img src="lang/zh_TW/flag.jpg" alt="" />'._('Chinese').'</a>');
-}
-
 function getTranslatedDownloadLink() {
     $lang = getLangParam();
     echo "downloads.php?lang=" . urlencode($lang);
 }
+
 ?>
