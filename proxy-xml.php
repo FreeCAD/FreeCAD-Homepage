50519d2532eac70dc5c567569207ef9143867da2
Anany <52584554+alanEG@users.noreply.github.com>
2025-10-26 14:25:03 -0700

    Update proxy-xml.php
    Enhanced URL safety checks by validating the scheme and added error handling for missing URLs.

--- proxy-xml.php
+++ proxy-xml.php
@@ -1,40 +1,36 @@
 <?php
-$whitelist = [
-    'blog.freecad.org'
-];
-
-function is_url_safe($url, $whitelist) {
-    $parsed = parse_url($url);
-    if (!$parsed || !isset($parsed['host'])) {
-        return false;
-    }
-
-    $host = strtolower($parsed['host']);
-
-    if (in_array($host, $whitelist)) {
+    $whitelist = [
+        'blog.freecad.org'
+    ];
+    
+    function is_url_safe($url, $whitelist) {
+        $parsed = parse_url($url);
+        if (!$parsed || !isset($parsed['host']) || !isset($parsed['scheme'])) {
+            return false;
+        }
+        
+        if (!in_array($parsed['scheme'], ['http', 'https'])) {
+            return false;
+        }
+    
+        $host = strtolower($parsed['host']);
+    
+        if (!in_array($host, $whitelist)) {
+            return false;
+        }
+    
         return true;
     }
-
-    $ip = gethostbyname($host);
-    if ($ip === $host) {
-        return false;
+    
+    $url = $_GET['url'] ?? '';
+    
+    if (empty($url)) {
+        echo "No URL provided";
+    } elseif (is_url_safe($url, $whitelist)) {
+        $response = file_get_contents($url);
+        header('Content-Type: application/xml');
+        echo $response;
+    } else {
+        echo "URL not allowed";
     }
-
-    if (in_array($ip, $whitelist)) {
-        return true;
-    }
-
-    return false;
-}
-
-$url = $_GET['url'] ?? '';
-if (empty($url)) {
-    echo "No URL provided";
-} elseif (is_url_safe($url, $whitelist)) {
-    $response = file_get_contents($url);
-    header('Content-Type: application/xml');
-    echo $response;
-} else {
-    echo "URL not allowed";
-}
 ?>
