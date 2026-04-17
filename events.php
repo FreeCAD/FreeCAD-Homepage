13e951c2b997a7ea3c73d3d0dddf8826289ff1fc
Chris Hennes <chennes@pioneerlibrarysystem.org>
2026-04-17 12:12:43 -0500

    Remove Google Calendar iframe from events.php
    Removed the Google Calendar iframe and retained the FreeCAD calendar iframe.

--- events.php
+++ events.php
@@ -60,15 +60,6 @@ updateLatestCategoryFromFeed(
           <p>
             <?php echo _('The calendar belows shows upcoming dates of FreeCAD meetings and other community events.'); ?>
           </p>
-          <!-- google widget
-          <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&showTitle=0&showPrint=0&showCalendars=0&src=NmU2Y2M4MTI2MDA1MWEzYzQ5ZmRmOTE1YzQ3MmYyZDYyNTc2ODIwMTI5ZjBlMGIyY2FjYjcwMjVlZDYyZjk2MEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23D50000"
-                  style="border:solid 1px #777"
-                  width="800" height="600"
-                  frameborder="0"
-                  scrolling="no">
-          </iframe>
-          -->
-            <iframe width="400" height="215" src="https://cloud.freecad.org/apps/calendar/embed/ZtJiizXtSqBaacwo"></iframe>
           <iframe id="open-web-calendar"
                   style="background:url('images/loader-freecad-small.gif') center center no-repeat;"
                   src="https://cloud.freecad.org/apps/calendar/embed/ZtJiizXtSqBaacwo"
